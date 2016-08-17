79a0896822ec343dc7d155a545990672ba9b37b3
Tom Offringa <tom.offringa@wearespindle.com>
2016-08-17 11:56:04 +0200

    Code cleanup and addons for a better prototype.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,28 @@
 'use strict';
 
-var autoprefixer = require('gulp-autoprefixer');
-var browserify = require('browserify');
-var browserSync = require('browser-sync');
-var buffer = require('vinyl-buffer');
-var gulp = require('gulp');
-var path = require('path');
-var sass = require('gulp-sass');
-var source = require('vinyl-source-stream');
-var sourcemaps = require('gulp-sourcemaps');
+let autoprefixer = require('gulp-autoprefixer');
+let browserify = require('browserify');
+let browserSync = require('browser-sync');
+let buffer = require('vinyl-buffer');
+let gulp = require('gulp');
+let path = require('path');
+let sass = require('gulp-sass');
+let source = require('vinyl-source-stream');
+let sourcemaps = require('gulp-sourcemaps');
 
-var paths = {
-    jsIndex: './assets/js/index.js',
+let paths = {
     browser: './',
-    img: './assets/images',
     fonts: './assets/fonts',
-    sassIndex: './assets/stylesheets/scss/styles.scss',
-    sass: './assets/stylesheets/**/*.scss',
-    js: './assets/js/**/*.js',
     html: './**/*.html',
+    img: './assets/images',
+    js: {
+        src: './assets/js/**/*.js',
+        jsIndex: './assets/js/index.js',
+    },
+    sass: {
+        src: './assets/stylesheets/**/*.scss',
+        sassIndex: './assets/stylesheets/scss/styles.scss',
+    },
 };
 
 
@@ -26,9 +30,9 @@ var paths = {
  * Generate the JavaScript bundled file. All source target files are wrapped in
  * a closure by Browserify.
  */
-gulp.task('js', function () {
-    return browserify(paths.jsIndex, {debug: true, extensions: ['es6']})
-        .transform("babelify", {presets: ["es2015"]})
+gulp.task('js', function() {
+    return browserify(paths.js.jsIndex, {debug: true, extensions: ['es6']})
+        .transform('babelify', {presets: ['es2015']})
         .bundle()
         .pipe(source('bundle.js'))
         .pipe(buffer())
@@ -42,9 +46,9 @@ gulp.task('js', function () {
 /**
  * Setup local server for static files.
  */
-gulp.task('browser-sync', function () {
+gulp.task('browser-sync', () => {
     const config = {
-        server: {baseDir: paths.browser}
+        server: {baseDir: paths.browser},
     };
 
     return browserSync(config);
@@ -55,16 +59,16 @@ gulp.task('browser-sync', function () {
  * Generate a compiled CSS file from the scss files. Add sourcemaps & autoprefix
  * files for the last two browser versions.
  */
-gulp.task('sass', function () {
-  return gulp.src(paths.sassIndex)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions']
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./compiled-assets/css'))
-    .pipe(browserSync.reload({stream: true}));
+gulp.task('sass', function() {
+    return gulp.src(paths.sass.sassIndex)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./compiled-assets/css'))
+        .pipe(browserSync.reload({stream: true}));
 });
 
 
@@ -72,12 +76,12 @@ gulp.task('sass', function () {
  * Watch sourcefiles and execute tasks upon changing the files, after that
  * refresh the browser.
  */
-gulp.task('watch', function () {
-    gulp.watch(paths.js, ['js']);
-    gulp.watch(paths.sass, ['sass']);
-    gulp.watch(paths.html, function () {
+gulp.task('watch', function() {
+    gulp.watch(paths.js.src, ['js']);
+    gulp.watch(paths.sass.src, ['sass']);
+    gulp.watch(paths.html, function() {
         return gulp.src('')
-            .pipe(browserSync.reload({stream: true}))
+            .pipe(browserSync.reload({stream: true}));
     });
 });
 
@@ -86,20 +90,20 @@ gulp.task('watch', function () {
  * Process all images. Currently they are only copied to the compiled-assets
  * directory. Later we can add some other tasks here.
  */
-gulp.task('images', function() {
-        return gulp.src(path.join(paths.img, '**'))
-        .pipe(gulp.dest('./compiled-assets/images'))
-        .pipe(browserSync.reload({stream: true}));
+gulp.task('images', () => {
+    return gulp.src(path.join(paths.img, '**'))
+    .pipe(gulp.dest('./compiled-assets/images'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 
 /**
  * Process all fonts.
  */
-gulp.task('fonts', function() {
-        return gulp.src(path.join(paths.fonts, '**'))
-        .pipe(gulp.dest('./compiled-assets/fonts'))
-        .pipe(browserSync.reload({stream: true}));
+gulp.task('fonts', () => {
+    return gulp.src(path.join(paths.fonts, '**'))
+    .pipe(gulp.dest('./compiled-assets/fonts'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('default', ['js', 'sass', 'watch', 'images', 'fonts', 'browser-sync']);
