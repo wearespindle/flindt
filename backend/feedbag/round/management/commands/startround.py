d83c6cf6da101075829681adcdcba3cc6459215c
Bob Voorneveld <bob.voorneveld@wearespindle.com>
2016-09-13 15:03:33 +0200

    Individual feedback is created on start round

--- backend/feedbag/round/management/commands/startround.py
+++ backend/feedbag/round/management/commands/startround.py
@@ -1,24 +1,17 @@
 from django.core.management import BaseCommand
 
-from ...manager import RoundManager, NoSolutionFound
+from ...manager import RoundManager
 from ...models import Round
 
 
 class Command(BaseCommand):
     help = """
-    import can currently be used to import users and roles from GlassFrog.
-
-    To import roles, a circle id should always be provided.
+    Helper command to start a feedback round, it will pick the first round in the DB.
     """
 
     def handle(self, *args, **options):
         round = Round.objects.first()
         manager = RoundManager(round)
-        try:
-            manager.start_round()
-        except NoSolutionFound:
-            print('')
-            print('fout!')
-        else:
-            print('')
-            print('success, tries: {}'.format(manager.tries))
+        manager.start_round()
+        print('')
+        print('success, tries: {}'.format(manager.tries))
