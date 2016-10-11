a0c0ce36d7e8100141278ea16a0ca371c0fa5d9d
Joris Engbers <info@jorisengbers.nl>
2016-10-11 10:14:24 +0200

    FEED-68: Send message when a round is started.

--- backend/feedbag/round/admin.py
+++ backend/feedbag/round/admin.py
@@ -11,6 +11,7 @@ class RoundAdmin(DjangoObjectActions, admin.ModelAdmin):
     def start_round(self, request, obj):
         round_manager = RoundManager(obj)
         round_manager.start_round()
+        obj.message_for_open()
 
     start_round.label = _('Start the round')
     start_round.short_description = "This will create feedback objects."
