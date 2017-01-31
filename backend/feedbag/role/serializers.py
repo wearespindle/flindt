7a900c4131918c508063893ccb827626de7ed9c6
Joris Engbers <info@jorisengbers.nl>
2017-01-31 14:58:25 +0100

    FEED-112: Serialize parent circle on a role.
    The solution looks and is pretty stupid.

--- backend/feedbag/role/serializers.py
+++ backend/feedbag/role/serializers.py
@@ -3,7 +3,29 @@ from rest_framework import serializers
 from .models import Role
 
 
+class ParentRoleSerializer(serializers.ModelSerializer):
+    """
+    Because we want to represent parent as a role, but a serializer/object can
+    not reference itself, ParentRoleSerializer is defined. This serializer is
+    in fact pretty much the same as :class:`RoleSerializer`.
+    """
+    class Meta:
+        model = Role
+        fields = (
+            'id',
+            'name',
+            'purpose',
+            'accountabilities',
+            'domains',
+            'parent',
+            'users',
+            'archived',
+        )
+
+
 class RoleSerializer(serializers.ModelSerializer):
+    parent = ParentRoleSerializer()
+
     class Meta:
         model = Role
         fields = (
