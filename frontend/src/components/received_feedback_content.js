6b5953ca2384e1ce0775acd0a2a5b6a05e9ba539
Tom Offringa <tom.offringa@wearespindle.com>
2017-02-01 14:46:38 +0100

    Fixed ESlint error by transforming component into stateless function

--- frontend/src/components/received_feedback_content.js
+++ frontend/src/components/received_feedback_content.js
@@ -5,99 +5,97 @@ import RatingRows from '../components/rating_row';
 
 require('moment/locale/nl');
 
-class ReceivedFeedbackContent extends Component {
+const ReceivedFeedbackContent = (props) => {
+    const { feedback, person, receiver, question } = props;
+    const accessToken = props.user.user.access_token;
 
-    render() {
-        const { feedback, person, receiver, question } = this.props;
-        const accessToken = this.props.user.user.access_token;
+    let table;
 
-        let table;
+    if (feedback.role) {
+        let role = feedback.role.role;
 
-        if (feedback.role) {
-            let role = feedback.role.role;
-
-            table = (
-                <table className="feedback-form--meta">
-                    <thead>
-                        <tr>
-                            <th>Person</th>
-                            <th>Role</th>
-                            <th>Subcircle</th>
-                            <th>Circle</th>
-                            <th>Received on</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td data-label="Person">
-                                { person.first_name } { person.last_name}
-                            </td>
-                            <td data-label="Role">
-                                <ModalButton accessToken={accessToken} role={role.id}>
-                                    { role.name }
-                                </ModalButton>
-                            </td>
-                            <td data-label="Subcircle">
-                                <ModalButton accessToken={accessToken} role={role.parent.id}>
-                                    { role.parent.name }
-                                </ModalButton>
-                            </td>
-                            <td data-label="Circle">
-                                Devhouse Spindle
-                            </td>
-                            <td data-label="Received on">
-                                <Time value={feedback.date} locale="EN" format="D MMMM YYYY" />
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            );
-        } else {
-            table = (
-                <table className="feedback-form--meta">
-                    <thead>
-                        <tr>
-                            <th>Person</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td data-label="Person">
-                                { person.first_name } { person.last_name}
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            );
-        }
-
-        return (
-            <div>
-                { table }
-
-                {
-                    feedback.role &&
-                        <RatingRows remarks={feedback.role.remarks} />
-                }
-
-                {
-                    feedback.individual &&
-                    <div className="feedback-form--row">
-                        <div className="feedback-form--answer-container">
-                            <strong>
-                                { feedback.individual.question.content }
-                            </strong>
-
-                            <div className="feedback-form--answer">
-                                <p>{feedback.individual.answer}</p>
-                            </div>
-                        </div>
-                    </div>
-                }
-            </div>
+        table = (
+            <table className="feedback-form--meta">
+                <thead>
+                    <tr>
+                        <th>Person</th>
+                        <th>Role</th>
+                        <th>Subcircle</th>
+                        <th>Circle</th>
+                        <th>Received on</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td data-label="Person">
+                            { person.first_name } { person.last_name}
+                        </td>
+                        <td data-label="Role">
+                            <ModalButton accessToken={accessToken} role={role.id}>
+                                { role.name }
+                            </ModalButton>
+                        </td>
+                        <td data-label="Subcircle">
+                            <ModalButton accessToken={accessToken} role={role.parent.id}>
+                                { role.parent.name }
+                            </ModalButton>
+                        </td>
+                        <td data-label="Circle">
+                            Devhouse Spindle
+                        </td>
+                        <td data-label="Received on">
+                            <Time value={feedback.date} locale="EN" format="D MMMM YYYY" />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    } else {
+        table = (
+            <table className="feedback-form--meta">
+                <thead>
+                    <tr>
+                        <th>Person</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td data-label="Person">
+                            { person.first_name } { person.last_name}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         );
     }
-}
+
+    return (
+        <div>
+            { table }
+
+            {
+                feedback.role &&
+                    <RatingRows remarks={feedback.role.remarks} />
+            }
+
+            {
+                feedback.individual &&
+                <div className="feedback-form--row">
+                    <div className="feedback-form--answer-container">
+                        <strong>
+                            { feedback.individual.question.content }
+                        </strong>
+
+                        <div className="feedback-form--answer">
+                            <p>{feedback.individual.answer}</p>
+                        </div>
+                    </div>
+                </div>
+            }
+        </div>
+    );
+};
+
 
 ReceivedFeedbackContent.propTypes = {
     feedback: React.PropTypes.object,
