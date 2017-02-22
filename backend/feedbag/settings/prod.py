b32e7db3083417533cc757ba1e3cdb77db237898
Egon Rijpkema <egon@voipgrid.nl>
2017-02-22 10:50:11 +0100

    Changes made to get production running.
    the contents of the live .env file are in team pasword.

--- backend/feedbag/settings/prod.py
+++ backend/feedbag/settings/prod.py
@@ -3,16 +3,14 @@ from feedbag.settings.base import *  # noqa
 DEBUG = boolean(os.environ.get('DEBUG', 0))
 TEMPLATE_DEBUG = boolean(os.environ.get('DEBUG', DEBUG))
 
-ALLOWED_HOSTS = ['.example.com']
-
 # Use the cached template loader so template is compiled once and read from
 # memory instead of reading from disk on each load.
-TEMPLATES[0]['OPTIONS']['loaders'] = [
-    ('django.template.loaders.cached.Loader', [
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader',
-    ]),
-]
+#TEMPLATES[0]['OPTIONS']['loaders'] = [
+#    ('django.template.loaders.cached.Loader', [
+#        'django.template.loaders.filesystem.Loader',
+#        'django.template.loaders.app_directories.Loader',
+#    ]),
+#]
 
 # HTTPS and Security Settings
 SECURE_HSTS_SECONDS = 31536000 # Future requests for the next year should use HTTPS only
